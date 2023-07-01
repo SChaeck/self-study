5821f9ecc79a0621ee986473e3ac541bcde123b0
Chaeck <zmfhtmgjsej@dgu.ac.kr>
2023-07-01 14:51:23 +0900

    23.07.01

--- 2580.py
+++ 2580.py
@@ -1,31 +1,31 @@
 arr = [list(map(int, input().split())) for _ in range(9)]
+zeroIdx = []
 
-def f(idx) :
+for i in range(81) :
+    if arr[i // 9][i % 9] == 0 : zeroIdx.append(i)
+
+def f(times) :
+    if times == len(zeroIdx) :
+        for i in range(9) :
+            print(" ".join(map(str, arr[i])))
+        exit()    
     
-    row = idx // 9
-    col = idx % 9
+    row = zeroIdx[times] // 9
+    col = zeroIdx[times] % 9
     
-    availNum = [1]*10 # 0~9까지 표현하는데 0은 걍 무시
-    for i in arr[row][:] :
-        availNum[i] = 0
-    for i in arr[:][col] :
-        availNum[i] = 0
-    for i in arr[((row) // 3) * 3 : ((row) + 3 // 3) * 3][((col) // 3) * 3 : ((col) + 3 // 3) * 3] :
-        availNum[i] = 0
+    availNum = [1]*10
+    for i in range(9) :
+        availNum[arr[row][i]] = 0
+        availNum[arr[i][col]] = 0
+    for i in arr[(row // 3) * 3 : ((row + 3) // 3) * 3] :
+        for j in i[(col // 3) * 3 : ((col + 3) // 3) * 3] : 
+            availNum[j] = 0
     
     for i in range(1, 10) :
         if availNum[i] == 1 :
             arr[row][col] = i
-            f(i)
+            f(times + 1)
             arr[row][col] = 0
 
-# 가로, 세로, 3x3칸에서 들어갈 수 있는 숫자를 추려서 
-# 넣고 재귀문 반복하면 될 것 같은데
-
-
-for i in range(81) :
-    if arr[i // 9][i % 9] == 0 : f(i) # 0을 찾고 
-    
-    ##### 0 위치를 찾고 그 위치를 저장하는 배열이 필요할듯
-    
+f(0)    
 
